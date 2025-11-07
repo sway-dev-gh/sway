a00cea1850799fda7b57304b1bb3d74799fe714b
Will Clinchard <infra.fm@outlook.com>
2025-11-07 08:59:44 -0800

    Fix production API URL - point to Render backend

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001'
+  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? 'https://sway-backend.onrender.com' : 'http://localhost:5001')
 })
 
 export default api
